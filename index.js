70b628809ccdedd0c28f5540a0aeb052c23fac56
IgorWL <61351943+IgorWL@users.noreply.github.com>
2020-02-22 11:08:17 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 const config = require("./config.json");
 const client = new Discord.Client();
-let active = new Map();
 
 const prefix = "botprefix";
 const ownerID = "yourid";
