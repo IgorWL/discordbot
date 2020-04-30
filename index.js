0b3bd28574b6489197b4a635611948ec60ab72a2
IgorWL <61351943+IgorWL@users.noreply.github.com>
2020-04-30 14:44:48 -0300

    Changed "config.json" to ".env"

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+
+
 const Discord = require("discord.js");
 const config = require("./config.json");
 const client = new Discord.Client();
@@ -45,4 +58,4 @@ Para saber meus comandos use: -help!`);
     console.log(e.stack);
   }
 });
-client.login(config.token);
+client.login(process.env.TOKEN);
